d7b3d838662c69697433c88b1c6559440db12b45
bruno-azzi <bruno-azzi@hotmail.com>
2025-06-17 02:50:29 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { FaBeer, FaInfo, FaInfoCircle } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>teste</h1>
+      {/* <Camera color="red" size={48} />
+      <Info />
+      <FileText /> */}
+
+      <FaInfoCircle  />
+    </div>
+  );
+}
